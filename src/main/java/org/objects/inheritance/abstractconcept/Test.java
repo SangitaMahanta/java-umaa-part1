724e611c66bd94eb87173583d8319a3b01d39062
SangitaMahanta <sangitamiki1998@gmail.com>
2020-10-19 08:01:53 +0530

    abstract class and abstract()

---
+++ src/main/java/org/objects/inheritance/abstractconcept/Test.java
@@ -0,0 +1,57 @@
+package org.objects.inheritance.abstractconcept;
+
+public class Test {
+    public static void main(String[] args) {
+//        Parent parent=new Parent();// abstract class have no object
+        Parent p = new Child();
+//      m2(10); // this m2() direct invoke the parent class m2()
+        p.m1();
+    }
+
+
+    // inner abstract class
+    private abstract static class Parent extends GrandParent {
+        // abstract class basically used in inheritance concept.
+        // we can't create object of an abstract class.
+        //Only we can define abstract() inside abstract class.
+        // abstract class always define with abstract keyword.
+        //abstract class can't be final.
+        //we can't define body of an abstract() method.
+        // abstract() method can be final but not private.
+        // child class must implement body of abstract() method.
+        // we can't define an abstract() as static also.
+
+        public abstract void m1();
+
+        public int m2(int x) {
+
+            int y = x + 10;
+            m1();
+            return y;
+        }
+    }
+
+    private static class Child extends Parent {
+        @Override
+        public void m1() {
+            m3();
+            System.out.println("in child class");
+        }
+
+        {
+            // this non-static block execute during at the time of object creation
+            int p = m2(10);
+            System.out.println(p);
+        }
+
+
+    }
+}
+
+// outer class
+class GrandParent {
+    void m3() {
+        System.out.println("In grandparent class");
+    }
+}
+
--- src/main/java/org/objects/inheritance/basicInheritance/Parent.java
+++ src/main/java/org/objects/inheritance/basicInheritance/Parent.java
@@ -13,6 +13,7 @@ public class Parent {
     public static void m1() {
         System.out.println("static m1() inside parent");
         System.out.println(x);
+
     }
 
     public void m2() {
